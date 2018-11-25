5f827e70d5f8eb472f6ce5a87fa7fa01a047a708
Maul <mprambadi@gmail.com>
2018-11-25 15:42:51 +0700

    Updates

---
+++ precache-manifest.a9e994081d77ea3f7614b2b4f27d8fc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6b3b642b4a4767f5db8",
+    "url": "/todo-lutfy/static/js/runtime~main.d6b3b642.js"
+  },
+  {
+    "revision": "04f23e6cf84a4764f0c2",
+    "url": "/todo-lutfy/static/js/main.04f23e6c.chunk.js"
+  },
+  {
+    "revision": "bdaab2806fd872d490f6",
+    "url": "/todo-lutfy/static/js/1.bdaab280.chunk.js"
+  },
+  {
+    "revision": "bdaab2806fd872d490f6",
+    "url": "/todo-lutfy/static/css/1.e5bea60d.chunk.css"
+  },
+  {
+    "revision": "3aa3fec0100ff482b044c23d42cc7774",
+    "url": "/todo-lutfy/index.html"
+  }
+];
